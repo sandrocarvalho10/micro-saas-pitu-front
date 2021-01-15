955ce5faf4af7fc6b85671d5a41440060c5b7de2
Sandro Carvalho <64166412+sandrocarvalho10@users.noreply.github.com>
2021-01-15 01:04:04 -0300

    Delete backend directory

--- backend/src/database.ts
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('mysql://root:teste1234@localhost:3306/micro_saas_pitu');
-
-export default sequelize;
-
--- backend/src/models/link.ts
+++
@@ -1,6 +0,0 @@
-export type Link = {
-  id?: number,
-  url: string,
-  code?: string,
-  hits?: number
-}
